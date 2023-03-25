a9b9e7fc02f81addcc3d1b832f033358107c1dc6
8toAutomate <70109587+8toAutomate@users.noreply.github.com>
2023-03-25 12:45:00 -0400

    added grid on camera

--- app.py
+++ app.py
@@ -5,14 +5,49 @@ app = Flask(__name__, template_folder="website")
 camera = cv2.VideoCapture(0)
 Cascade = cv2.CascadeClassifier('fist.xml')
 fistCoords = []
+shape = []
+
+# draws a grid on the camera to guide the user
+def drawGrid(shape, frame):
+    x = shape[0]
+    y = shape[1]
+
+    # 7x4
+
+    yIntervals = []
+    xIntervals = []
+
+    yCounter = 0
+    xCounter = 0
+
+    for i in range(0,7):
+        yIntervals.append(yCounter)
+        yCounter = yCounter + y/7
+
+        xIntervals.append(xCounter)
+        xCounter = xCounter + x/4
+    
+    cv2.line(frame, (yIntervals[0],0), (yIntervals[0], y))
+    cv2.line(frame, (yIntervals[1],0), (yIntervals[1], y))
+
+    for i in yIntervals:
+        cv2.line(frame, (i, 0), (i, y), (255,0,0), 2)
+
+    for e in xIntervals:
+        cv2.line(frame, (0, e), (x, e), (255,0,0), 2)
+
+
 
 def generate_frames():
     global fistCoords
+    global shape
     while True:
         success, frame = camera.read() #read camera frame: return bool and frame
         if not success:
             break
         else:
+
+            shape = frame.shape()
 
             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
@@ -22,7 +57,9 @@ def generate_frames():
                 cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
                 fistCoords = [x + w/2, y + h/2] # location of the fist
             
-            cv2.putText(frame, "x: %s, y: %s" % (fistCoords[0], fistCoords[1]))
+            cv2.putText(frame, str(fistCoords), (30,30))
+
+            drawGrid(shape, frame)
 
             ret, buffer = cv2.imencode('.jpg', frame)
             frame = buffer.tobytes()
